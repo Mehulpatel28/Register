3e19f0927bb3c08f9ec60a4d0e4021b6cf03ff10
mehulpatel <mehulpatel6199@gmail.com>
2021-04-13 03:37:04 -0600

    class component

--- src/screen/SplashScreen/SplashScreen.js
+++ src/screen/SplashScreen/SplashScreen.js
@@ -1,16 +1,20 @@
-import React from 'react'
-import { View,Image} from 'react-native'
+import React, { Component } from 'react'
+import { View,Image } from 'react-native'
 
-export default function SplashScreen({navigation}) {
-    setTimeout(() => {
-        navigation.navigate('Login');
-      }, 4000);
-    return (
-        <View>
-        <Image
-        source={require('../../../src/assets/splash.gif')}
-        style={{height: 740, width: '100%'}}
-        />
-        </View>
-    )
+class SplashScreen extends Component {
+    render() {
+        setTimeout(() => {
+                    this.props.navigation.navigate('Login');
+                  }, 4000);
+        return (
+            <View>
+                <Image
+                    source={require('../../../src/assets/splash.gif')}
+                    style={{ height: 740, width: '100%' }}
+                />
+            </View>
+        )
+    }
 }
+
+export default SplashScreen
